82e4f7b86c3343f79502a360ead1174428ca52b0
Claudineicrdn <48592108+Claudineicrdn@users.noreply.github.com>
2026-01-28 01:06:40 +0000

    final

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -3,11 +3,21 @@ export function Welcome() {
   const titulo = 'Hello Ocean!';
   
   return (
-    <div>
-      <h1>
-        {titulo}
-      </h1>
-    </div>    
+    <>
+      <div className="header">
+        <h1>
+          {titulo}
+        </h1>
+      </div>
+      <div className="div-link">
+        <img className="img-link" src="https://i.pinimg.com/originals/68/07/bf/6807bf57a06e266c5fb07a4e81d37823.jpg" />
+        <p>De: R$ 50,00</p>
+        <p>Por: R$ 25,00</p>
+      </div> 
+      <div className="botao-comprar">
+        <button>Comprar</button>
+      </div>   
+    </>
   );
 }
 
